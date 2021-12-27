674a60dba7ca1506287290ffc2c976e355f13212
V1CTOR-cloud <latipicacuenta89@gmail.com>
2021-12-28 00:35:27 +0100

    subiendo cambios

--- JS/index.js
+++ JS/index.js
@@ -1,24 +1,18 @@
-let fondo = document.querySelector('.imagen');
-let dia_noche = document.querySelector('.dia_noche');
-let menu = document.querySelector('.menuToggle');
-let body = document.querySelector('body');
-let nav = document.querySelector('.nav');
+window.onscroll = () =>{
 
-dia_noche.onclick = function() {
-    dia_noche.classList.toggle('active');
-    body.classList.toggle('dark');
-    fondo.setAttribute("src", "../img/principal_oscuro.png");
-    if (dia_noche.classList.contains('active')) {} else {
-        fondo.setAttribute("src", "../img/principal_claro.png");
-        cont2.classList.remove('dark');
+    if (window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
     }
+
 }
 
-menu.onclick = function() {
-    menu.classList.toggle('active');
-    nav.classList.toggle('active');
-}
-
-nav.onclick = function() {
-    nav.classList.toggle
+window.onload = () => {
+    
+    if (window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
 }
