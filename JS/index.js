aabd8e99ae11a3d1b604b900d681f1a033fbc400
V1CTOR-cloud <latipicacuenta89@gmail.com>
2022-01-06 12:22:01 +0100

    Añadiendo apartado de servicios

--- JS/index.js
+++ JS/index.js
@@ -98,3 +98,25 @@ let swiper3 = new Swiper(".blogs-slider", {
     },
   },
 });
+
+let swiper4 = new Swiper(".servicios-slider", {
+  spaceBetween:10,
+  grabCursor: true,
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
