97f9fb3f39cc91368670a18d0f454920eeaca45c
PauSanCli <pauglu777@gmail.com>
2022-02-21 19:24:08 +0100

    añadido symfony y peticion de js

---
+++ subscribeEmail.php
@@ -0,0 +1,10 @@
+<?php     
+
+
+$to_email = 'name @ company . com';
+$subject = 'Testing PHP Mail';
+$message = 'This mail is sent using the PHP mail function';
+$headers = 'From: noreply @ company . com';
+mail($to_email,$subject,$message,$headers);
+    
+?>
